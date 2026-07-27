e431ad666102fb7f5a884debdca78cadbd25585c
Artgirl10301 <101661829+Artgirl10301@users.noreply.github.com>
2026-07-26 22:59:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,20 +5,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const totalSlides = slides.length;
 
     function showSlides() {
-        // Hide all slides
         slides.forEach(slide => {
             slide.style.opacity = 0;
-            slide.style.zIndex = -1;
         });
 
-        // Show current slide
         slides[slideIndex].style.opacity = 1;
-        slides[slideIndex].style.zIndex = 1;
 
-        // Move to next slide
         slideIndex = (slideIndex + 1) % totalSlides;
 
-        setTimeout(showSlides, 6000); // 6 seconds per slide
+        setTimeout(showSlides, 6000);
     }
 
     showSlides();
